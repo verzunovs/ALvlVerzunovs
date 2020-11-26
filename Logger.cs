1687715407ecd632d06cfc061b633b37c3db0099
Serhii Verzunov <shvabroid1221@gmail.com>
2020-11-26 20:09:47 +0200

    Added singleton to class Logger

--- Logger.cs
+++ Logger.cs
@@ -8,5 +8,13 @@ namespace Hw1M2
 {
     class Logger
     {
+        private static Logger instance;
+        StringBuilder log = new StringBuilder();
+        private Logger() { }
+        public static Logger GetInstance()
+        {
+            if (instance == null) instance = new Logger();
+            return instance;
+        }
     }
 }
